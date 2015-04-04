f988882d40946877d578e2b85ac6965ae9edaec3
sealionkat <sealionkat@gmail.com>
2015-04-05 00:52:42 +0200

    part of tokenizer

--- svm/Tokenizer/Tokenizer.cs
+++ svm/Tokenizer/Tokenizer.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace MiniSVM.Tokenizer
 {
     public class Tokenizer
     {
+        private string[] uselessWords = null;
+
+        public void readUselessWords(string filename)
+        {
+            var readedTxt = File.OpenText(filename);
+        }
+
+        public string[] tokenizeString(string filename)
+        {
+            return null;
+        }
+
+
     }
 }
